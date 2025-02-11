193d7815a170a58785f61a01a9614db57da936f0
Aaron Dweck <aarondweck24@gmail.com>
2025-02-11 11:04:49 +0000

    added chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
   )
 }
 
