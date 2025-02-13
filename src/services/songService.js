2ba844e6a86ccf1c9a405b3993e7a144b2439e2c
Aaron Dweck <aarondweck24@gmail.com>
2025-02-13 13:20:24 +0000

    Added update song and modified the audio upload to send the duration in seconds

--- src/services/songService.js
+++ src/services/songService.js
@@ -1,3 +1,4 @@
+import { getToken } from "../utils/auth.js";
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL + '/songs'
@@ -6,16 +7,38 @@ export async function songCreate(formData) {
     try {
         const res = await axios.post(BASE_URL, formData, {
             headers: {
-                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY3YWExZDIxYTk4M2NkYmU5NGY0YjllNyIsInVzZXJuYW1lIjoiYWFyb24iLCJwcm9maWxlSW1hZ2UiOiJpbWFnZSIsImlzQXJ0aXN0Ijp0cnVlLCJwbGF5bGlzdHMiOltdLCJsaWtlcyI6W119LCJpYXQiOjE3MzkzNjM3ODYsImV4cCI6MTczOTQ1MDE4Nn0.I9SK3S1-k8kOyZWHwgQ1LmWXbYbwXUpnw9_1rExXZac`
+                Authorization: `Bearer ${getToken()}`
             }
-        } )
-        console.log('service 1:', res)
+        })
         return res.data
     } catch (error) {
-        console.log('service 2:', error.response.data)
+        console.log(error)
         throw error
     }
-    
+}
+
+export async function songShow(id) {
+    try {
+        const res = await axios.get(BASE_URL + `/${id}`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+export async function songUpdate(id, formData) {
+    try {
+        const res = await axios.put(BASE_URL + `/${id}`, formData, {
+            headers: {
+                Authorization: `Bearer ${getToken()}`
+            }
+        })
+        return res.data
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
 }
 
 export const getAllSongs = () => axios.get(BASE_URL);
