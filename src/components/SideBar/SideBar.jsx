3ff4d1f1835616121fd9282aeec8554fbc722270
Aaron Dweck <aarondweck24@gmail.com>
2025-02-14 00:48:22 +0000

    Added side bar and fixed auth background

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,36 @@
-export default function SideBar () {
+import { NavLink } from "react-router"
+import { UserContext } from "../../contexts/UserContext"
+import { useContext } from "react"
+import styles from './Sidebar.module.css'
+
+export default function SideBar() {
+
+    const { user } = useContext(UserContext)
+
     return (
-        <h1>SideBar</h1>
+        <>
+            {user ?
+                <>
+                    <p className={styles.libaryTitle}>Your Libary</p>
+                    <div className={styles.promtCard}>
+                        <p className={styles.promtTitle}>Time to make some music!</p>
+                        <p className={styles.promtBody}>Start a new song and let your creativity shine.</p>
+                        <div className={styles.authButtons}>
+                            <NavLink className={styles.sidebarBtn} to='/songs/new' >Create a song</NavLink>
+                        </div>
+                    </div>
+                </>
+                :
+                <div className={styles.promtCard}>
+                    <p className={styles.promtTitle}>Ready to create your next hit?</p>
+                    <p className={styles.promtBody}>Log in or register to start sharing your music with the world!</p>
+                    <div className={styles.authButtons}>
+                        <NavLink className={styles.sidebarBtn} to='/login' >Login</NavLink>
+                        <NavLink className={styles.sidebarBtn} to='/register' >Register</NavLink>
+                    </div>
+                </div>
+            }
+        </>
+
     )
 }
